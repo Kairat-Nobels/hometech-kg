035d1a9dbd24eb79ae6c0df80a5914a73ed96b7a
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2026-06-19 09:53:08 +0600

    change api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
-// const api = "https://89c8ee237c686207.mokky.dev/";
-const api = "https://6a294142f59cb8f65f1cc24f.mockapi.io/";
+const api = "https://89c8ee237c686207.mokky.dev/";
+// const api = "https://6a294142f59cb8f65f1cc24f.mockapi.io/";
 export const itemsApi = api + 'items'
 export const ordersApi = api + 'orders'
 export const reviewsApi = api + 'reviews'
